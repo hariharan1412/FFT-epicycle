17ae4cd2cbb909539eed8188f3b53df98496f783
Devil1412 <hariharan141200@gmail.com>
2022-11-24 14:22:03 +0530

    HELLO WORLD

--- coordinates.py
+++ coordinates.py
@@ -1,32 +1,32 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('/home/hariharan/project/FFT/images/1.jpg', 0)
+img = cv2.imread('YOUR PATH', 0)
 edges = cv2.Canny(img, 100, 255)  
 
 indices = np.where(edges != [0])
 coordinates = zip(indices[0], indices[1])
 
-# with open('co.txt' , 'a') as f:
+with open('co.txt' , '+a') as f:
 
-#     f.writelines("x = [")
+    f.writelines("x_ = [")
 
-#     for i in coordinates:
-
-#         f.writelines(',')
-#         f.writelines(str(i[0]))
-
-#     f.writelines("]")
-
-#     f.writelines('\n')
-
-with open('co.txt' , 'a') as f:
-
-    f.writelines("y = [")
-    
     for i in coordinates:
 
         f.writelines(',')
-        f.writelines(str(i[1]))
+        f.writelines(str(i[0]))
 
     f.writelines("]")
+
+    f.writelines('\n')
+
+with open('co.txt' , '+a') as k:
+
+    k.writelines("y_ = [")
+    
+    for i in coordinates:
+
+        k.writelines(',')
+        k.writelines(str(i[1]))
+
+    k.writelines("]")
