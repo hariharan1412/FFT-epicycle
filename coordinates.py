a0537e8faa32b0f1262a3eb7d1052534c722b401
hariharan <hariharan141200@gmail.com>
2022-11-24 17:48:25 +0530

    cor

--- coordinates.py
+++ coordinates.py
@@ -1,24 +1,32 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('/home/hariharan/project/FFT/iamges/2.jpg', 0)
+img = cv2.imread('/home/hariharan/project/FFT/images/1.jpg', 0)
 edges = cv2.Canny(img, 100, 255)  
 
 indices = np.where(edges != [0])
 coordinates = zip(indices[0], indices[1])
 
-n = 0
+# with open('co.txt' , 'a') as f:
 
+#     f.writelines("x = [")
 
-with open('co.txt' , 'w') as f:
+#     for i in coordinates:
 
-    for j in coordinates:
-        f.writelines(str(j[0]))
-        f.writelines(" , ")
-   
-# with open('y_cor.js' , 'w') as f:
+#         f.writelines(',')
+#         f.writelines(str(i[0]))
 
-#     for j in coordinates:
-#         f.writelines(str(j[1]))
-#         f.writelines(" , ")
-        
+#     f.writelines("]")
+
+#     f.writelines('\n')
+
+with open('co.txt' , 'a') as f:
+
+    f.writelines("y = [")
+    
+    for i in coordinates:
+
+        f.writelines(',')
+        f.writelines(str(i[1]))
+
+    f.writelines("]")
